da089e8366c1d137da24a71498ab9dff2fd22ed8
rudyardmoreno <rudyard@morenojasso.com>
2017-05-22 16:00:41 -0600

    Ups

--- episodic-events/src/main/java/com/example/episodicevents/events/controller/EventController.java
+++ episodic-events/src/main/java/com/example/episodicevents/events/controller/EventController.java
@@ -21,22 +21,14 @@ public class EventController {
     EventRepository eventRepository;
 
     @GetMapping("/recent")
-    public List<Event> getProducts() {
+    public List<Event> getRecentEvents() {
 
         List<Event> eventList = new ArrayList<Event>();
         eventList=eventRepository.findAll();
 
         return eventList;
-        /*return Arrays.asList(
-                new Play(52,987,456,null,new DataDefault(0)),
-                new Pause(52,987,456,null,new DataDefault(1023)),
-                new FastForward(52,987,456,null,new ForwardData(4,408,2.5)),
-                new Rewind(52,987,456,null,new ForwardData(4,408,2.5)),
-                new Progress(52,987,456,null,new DataDefault(4)),
-                new Scrub(52,987,456,null,new ScrubData(4,408))
-        );*/
     }
-
+    
     @PostMapping("/")
     public Object createProduct(@RequestBody Event event) {
         return eventRepository.save(event);
