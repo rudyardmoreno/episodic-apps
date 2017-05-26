456ab76488bb2629a4f6c48104a00693439944d3
rudyardmoreno <rudyard@morenojasso.com>
2017-05-26 08:45:31 -0600

    upd

--- episodic-events/src/main/java/com/example/episodicevents/DemoApplication.java
+++ episodic-events/src/main/java/com/example/episodicevents/DemoApplication.java
@@ -3,7 +3,9 @@ package com.example.episodicevents;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
 @SpringBootApplication
 @EnableDiscoveryClient
 public class DemoApplication {
--- episodic-gateway/src/main/java/com/example/episodicgateway/DemoApplication.java
+++ episodic-gateway/src/main/java/com/example/episodicgateway/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.episodicshows;
+package com.example.episodicgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy
-@SpringBootApplication
 @EnableDiscoveryClient
+@SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
